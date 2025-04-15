ebacbe6434dfdc6eae263a0eea331b3737e87efb
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-04-15 13:12:56 +0200

    test travis_setup_go.bash

---
+++ travis_setup_go.bash
@@ -0,0 +1,29 @@
+itravis_setup_go() {
+  set -x  # Włączenie wyświetlania szczegółowego wykonywania poleceń
+
+  local go_version="${TRAVIS_GO_VERSION:-${1}}"
+  local go_import_path="${TRAVIS_GO_IMPORT_PATH:-${2}}"
+
+  if [[ ! "${go_version}" ]]; then
+    echo 'Missing TRAVIS_GO_VERSION' >&2
+    return 86
+  fi
+
+  if [[ ! "${go_import_path}" ]]; then
+    echo 'Missing TRAVIS_GO_IMPORT_PATH' >&2
+    return 86
+  fi
+
+  travis_cmd "export GOPATH=\"${TRAVIS_HOME}/gopath\"" --echo
+  travis_cmd "export PATH=\"${TRAVIS_HOME}/gopath/bin:${PATH}\"" --echo
+  travis_cmd "export GO111MODULE=\"${GO111MODULE}\"" --echo
+
+  go install "golang.org/dl/go${go_version}@latest" --echo
+  "go${go_version}" download --echo
+  sudo ln -s "${TRAVIS_HOME}/gopath/bin/go${go_version}" "${TRAVIS_HOME}/gopath/bin/go" --echo
+  travis_cmd "export GOROOT=$(go"${go_version}" env GOROOT)" --echo
+  travis_cmd "export PATH=${GOROOT}/bin:${PATH}" --echo
+  mkdir -p "$(dirname "${GOPATH}/src/${go_import_path}")"
+  ln -s "${TRAVIS_BUILD_DIR}" "$GOPATH/src/${go_import_path}"
+}
+
